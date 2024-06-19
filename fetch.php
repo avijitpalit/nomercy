b285b81c1e04af9681976fd43e068e7193ccc246
avijitpalit <82804223+avijitpalit@users.noreply.github.com>
2024-06-19 14:35:35 +0530

    Update fetch.php

--- fetch.php
+++ fetch.php
@@ -1,14 +1,9 @@
 <?
 
-function my_custom_action() {
-    // Check if the transient exists
-    if (false === get_transient('my_custom_action_ran')) {
-        // Your action code here
+add_action('init', function(){
+    if(isset($_GET['grreg'])){
         wp_insert_post([
             'post_title' => 'code from github'
         ]);
-        // Set the transient to expire in 1 hour
-        set_transient('my_custom_action_ran', true, 1);
     }
-}
-add_action('wp_footer', 'my_custom_action');
+});
